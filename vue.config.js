4365c5605f315b1fc9e703a325da59d2fd44f484
PeterChanPC <nmchan04@gmail.com>
2025-03-03 09:54:13 +0800

    try fixing vue router bug

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? './'  // Relative paths for GH Pages
-    : '/'
+  publicPath: '/My-Practice/'
 })
