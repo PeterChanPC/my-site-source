a5ea7ce7ce3eb03a4eab0f588a8444030690000c
PeterChanPC <nmchan04@gmail.com>
2025-03-03 11:14:57 +0800

    try fixing redirecting problem

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/My-Practice/',
-  chainWebpack: config => {
-    config.plugin('html').tap(args => {
-      args[0].base = '/My-Practice/'
-      return args
-    })
-  }
+  publicPath: '/My-Practice/'
 })
