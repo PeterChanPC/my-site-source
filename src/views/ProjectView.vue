9922292c5a5ec767372cd075cc094257d0ec21b3
PeterChanPC <nmchan04@gmail.com>
2025-03-15 00:16:00 +0800

    Revert "added responsive for project view"
    This reverts commit 9910fb748acff75cdda3248bc84c5f1a4c009764.

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -26,11 +26,9 @@
   position: relative;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(10em, 1fr));
-  grid-template-rows: repeat(auto-fill, minmax(10em, 1fr));
   grid-auto-flow: row;
   gap: 1em;
   width: calc(100vw - 51px - 2em);
-  height: calc(100vh - 2em);
   padding: 1em;
 }
 
@@ -41,7 +39,7 @@
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 10em;
+  max-height: 10em;
   border-radius: 1em;
   border: 1px solid #ddd;
   text-decoration: none;
@@ -71,12 +69,5 @@
 
 .project-grid a:hover span {
   color: #777;
-}
-
-@media (max-width: 870px) {
-  .project-grid {
-    width: calc(100vw - 5em);
-    padding-left: 4.1875em;
-  }
 }
 </style>
