8e179211067908f1068e51b41e7ba042802e2574
PeterChanPC <nmchan04@gmail.com>
2025-04-02 23:07:06 +0800

    try hosting github pages again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/My-Practice",
+  base: "/",
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: { 
