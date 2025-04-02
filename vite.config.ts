efb16d76517b6341821ea3053574aabaa6ddad68
PeterChanPC <nmchan04@gmail.com>
2025-04-02 23:12:47 +0800

    try hosting github pages again

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: { 
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('src', import.meta.url))
     }
   }
 })
