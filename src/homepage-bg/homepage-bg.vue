51e05ff98b3938f58a8b4a4c3af9b26aa0c1bd5a
PeterChan PC <nmchan04@gmail.com>
2025-06-06 16:44:47 +0800

    chore: update homepage-bg

--- src/homepage-bg/homepage-bg.vue
+++ src/homepage-bg/homepage-bg.vue
@@ -18,19 +18,29 @@ export default defineComponent({
       const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
       const renderer = new THREE.WebGLRenderer({
         canvas: canvas.value,
+        antialias: true,
       });
-      renderer.setSize(window.innerWidth, window.innerHeight);
-
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-      camera.position.z = 5;
       
+      const material = new THREE.MeshPhongMaterial({color: 0xffffff});
+      const geometry = new THREE.SphereGeometry(1, 32, 32);
+      const sphere = new THREE.Mesh(geometry, material);
+
+      const ambientLight = new THREE.AmbientLight(0xffffff);
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+      directionalLight.position.set(10, 10, 10);
+      directionalLight.target.position.set(0, 0, 0);
+
+      scene.add(sphere);
+      scene.add(ambientLight);
+      scene.add(directionalLight);
+
+      camera.position.z = 5;
+
       function animate() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.render(scene, camera);
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
       };
       renderer.setAnimationLoop(animate);
     });
