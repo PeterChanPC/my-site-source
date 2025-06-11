e728bc0a7b0767127d90bc4d72a51b292448467c
PeterChanPC <nmchan04@gmail.com>
2025-06-12 00:54:09 +0800

    feat: add raycasting in homepage-bg for collision detection

--- src/views/homepage/homepage-bg.vue
+++ src/views/homepage/homepage-bg.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, Ref, ref, render } from 'vue';
+import { defineComponent, onMounted, onUnmounted, Ref, ref } from 'vue';
 import * as THREE from 'three';
 
 export default defineComponent({
@@ -13,7 +13,29 @@ export default defineComponent({
   setup() {
     const canvas: Ref<HTMLCanvasElement | THREE.OffscreenCanvas | undefined> = ref(undefined);
 
+    const moveDir = new THREE.Vector3(0, 0, 0);
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case 'ArrowUp':
+          moveDir.set(0, 0, -1);
+          break;
+        case 'ArrowDown':
+          moveDir.set(0, 0, 1);
+          break;
+        case 'ArrowLeft':
+          moveDir.set(-1, 0, 0);
+          break;
+        case 'ArrowRight':
+          moveDir.set(1, 0, 0);
+          break;
+        default:
+          moveDir.set(0, 0, 0);
+      };
+    };
+
     onMounted(() => {
+      // setup renderer
       const renderer = new THREE.WebGLRenderer({
         canvas: canvas.value,
         antialias: true,
@@ -21,62 +43,102 @@ export default defineComponent({
       renderer.shadowMap.enabled = true;
       renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
+      // setup scene
       const scene = new THREE.Scene();
 
-      let aspect = renderer.domElement.width / renderer.domElement.height;
-      const camera = new THREE.OrthographicCamera(-5, 5, 5 / aspect, -5 / aspect);
+      // setup camera
+      let aspect = window.innerWidth / window.innerHeight;
+      const camera = new THREE.OrthographicCamera(-5, 5, 5 / aspect, -5 / aspect, -10, 100);
       camera.position.set(0, 1, 5);
       camera.lookAt(0, 0, 0);
 
-      const material_1 = new THREE.MeshStandardMaterial({color: 0xffffff});
-      const material_2 = new THREE.MeshStandardMaterial({color: 0xeeeeee});
-      const material_3 = new THREE.MeshStandardMaterial({color: 0xdddddd});
+      // setup materials
+      const material_1 = new THREE.MeshStandardMaterial({ color: 0xffffff });
+      const material_2 = new THREE.MeshStandardMaterial({ color: 0xeeeeee });
+      const material_3 = new THREE.MeshStandardMaterial({ color: 0xeeeeee });
 
+      // setup wall object
+      const wallGeometry = new THREE.PlaneGeometry(100, 100);
+      const wall = new THREE.Mesh(wallGeometry, material_2);
+      wall.position.set(0, 0, -8);
+      wall.receiveShadow = true;
+
+      // setup floor object
+      const floorGeometry = new THREE.BoxGeometry(100, 20, 20);
+      const floor = new THREE.Mesh(floorGeometry, material_3);
+      floor.position.set(0, -11, 0);
+      floor.rotation.set(-Math.PI / 2, 0, 0);
+      floor.receiveShadow = true;
+
+      //setup lightings
+      const ambientLight = new THREE.AmbientLight(0xdddddd);
+
+      const spotLight = new THREE.SpotLight(0xffffff);
+      spotLight.power = 50000;
+      spotLight.angle = 0.1;
+      spotLight.penumbra = 0.8;
+      spotLight.castShadow = true;
+      spotLight.position.set(50, 50, 50);
+
+      // setup sphere object
       const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
       const sphere = new THREE.Mesh(sphereGeometry, material_1);
       sphere.position.set(2, 0, -2);
       sphere.castShadow = true;
       sphere.receiveShadow = true;
 
-      const wallGeometry = new THREE.PlaneGeometry(100, 100);
-      const wall = new THREE.Mesh(wallGeometry, material_2);
-      wall.position.set(0, 0, -5);
-      wall.receiveShadow = true;
-      
-      const floorGeometry = new THREE.BoxGeometry(100, 10, 20);
-      const floor = new THREE.Mesh(floorGeometry, material_3);
-      floor.position.set(0, -11.1, 0);
-      floor.rotation.set(-Math.PI / 2, 0, 0);
-      floor.receiveShadow = true;
+      // setup raycaster
+      const raycaster = new THREE.Raycaster(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), 0, 1);
 
-      const ambientLight = new THREE.AmbientLight(0xdddddd);
+      function rayCast() {
+        raycaster.set(sphere.position, moveDir);
+      };
 
-      const spotLight = new THREE.SpotLight(0xffffff);
-      spotLight.power = 300;
-      spotLight.angle = 0.5;
-      spotLight.penumbra = 0.3;
-      spotLight.castShadow = true;
-      spotLight.position.set(5, 5, 4);
+      // setup user movement controller
+      window.addEventListener('keydown', handleKeyDown);
 
+      function moveObject() {
+        const speed = 0.2;
+        sphere.position.add(moveDir.multiplyScalar(speed));
+      };
+
+      // apply elements to scene
       scene.add(sphere);
       scene.add(floor);
       scene.add(wall);
       scene.add(ambientLight);
       scene.add(spotLight);
 
-      function animate() {
-        aspect = renderer.domElement.width / renderer.domElement.height;
+      // setup animation update
+      let clock = new THREE.Clock();
+      let delta = 0;
+      let interval = 1 / 60; // 60fps
+
+      function update() {
+        delta += clock.getDelta();
+
+        aspect = window.innerWidth / window.innerHeight;
         camera.top = 5 / aspect;
         camera.bottom = -5 / aspect;
-
         camera.updateProjectionMatrix();
+
+        if (delta > interval) { // fixed fps update
+          rayCast();
+          if (raycaster.intersectObjects(scene.children).length === 0) moveObject();
+          delta = delta % interval;
+        };
+
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.render(scene, camera);
       };
-      renderer.setAnimationLoop(animate);
+      renderer.setAnimationLoop(update);
     });
 
-    return { canvas };
+    onUnmounted(() => {
+      window.removeEventListener('keydown', handleKeyDown);
+    });
+
+    return { canvas, handleKeyDown };
   },
 });
 </script>
