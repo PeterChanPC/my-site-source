d0d60bd4451da8028a89b1ea226737073563eb5b
PeterChan PC <nmchan04@gmail.com>
2025-07-04 11:43:30 +0800

    refactor: remove theme.scss, refactor theme css into App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,43 @@
 <script setup lang="ts">
 import GlobalHeader from '@/layout/global-header/global-header.vue';
 import GlobalSidebar from '@/layout/global-sidebar/global-sidebar.vue';
-import { useTemplateRef } from 'vue';
+import { useTemplateRef, watchEffect } from 'vue';
+import { useThemeStore } from './stores/theme.store';
+
 const sidebar = useTemplateRef('sidebar');
+const themeStore = useThemeStore();
+
+watchEffect(() => {
+  document.body.style.setProperty('--theme', themeStore.theme);
+});
 </script>
 
 <style lang="scss">
+@use '@/styles/style';
+
 body {
   margin: 0;
 }
 
+@container style(--theme: light) {
+  #app {
+    --bg-color: #{style.$bg-light};
+    --bg-color-opague: #{style.$bg-light-opague};
+    --txt-color: #{style.$txt-light};
+  }
+}
+
+@container style(--theme: dark) {
+  #app {
+    --bg-color: #{style.$bg-dark};
+    --bg-color-opague: #{style.$bg-dark-opague};
+    --txt-color: #{style.$txt-dark};
+  }
+}
+
 #app {
+  @include style.transition((background-color, color), 100ms);
+  container-type: normal;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   position: relative;
   overflow: hidden;
